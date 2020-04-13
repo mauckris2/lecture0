79ab65d846ce84635447daa95903089cea49630d
Mauckris2 <mau_ckris@yahoo.com>
2020-04-13 11:20:58 +1000

    python basic

---
+++ name.py
@@ -0,0 +1,30 @@
+name = input()
+print(f"hello, {name}!")
+i=28
+print(f"i is {i}")
+
+f=2.8
+print(f"f is {f}")
+
+b=True
+print(f"b is {b}")
+
+n=None
+print(f"n is {n}")
+
+
+x = 28
+
+if x >0:
+	print("x is positive")
+elif x< 0:
+	print("x is negative")
+else:
+	print("x is zero")
+
+name1 = "Alice"
+cordinates = (10.0,20.0)
+names = ["Alice","Bob","Charlie","Maurice"]
+
+for neym in names:
+	print(neym)
